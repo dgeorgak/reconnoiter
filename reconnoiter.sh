0d944f7db892fd2b9a23428b885e2455ef6af102
dgeorgak <dimgeorgak@pm.me>
2025-03-21 21:01:56 +0200

    code refinement

--- reconnoiter.sh
+++ reconnoiter.sh
@@ -30,18 +30,25 @@ subfinder -d $domain > $subdomain_path/found.txt
 
 # Get subdomains with assetfinder
 echo -e "${RED} [+] Running assetfinder... ${RESET}"
-assetfinder $domain | grep $domain >> $subdomain_path/found.txt
+assetfinder $domain >> $subdomain_path/found.txt
 
-# Get subdomains with assetfinder
-echo -e "${RED} [+] Running amass... ${RESET}"
-amass enum -d $domain > $subdomain_path/amass_output.txt
-cat $subdomain_path/amass_output.txt | grep $domain | cut -d ' ' -f1 >> $subdomain_path/found.txt
+# Get subdomains with amass
+# adding "-r [DNS]" to avoid errors if run within a VM
+echo -e "${RED} [+] Running amass... ${RESET}this might take awhile :|"
+amass enum -active -d $domain -r 8.8.8.8 > $subdomain_path/amass_output.txt
+cat $subdomain_path/amass_output.txt | cut -d ' ' -f1 >> $subdomain_path/found.txt
 
-# Sort and remove duplicates, use httprobe to find the alive subdomains
+# Sort and remove duplicates, seperate between domain and non-domain findings
 echo -e "${RED} [+] Finding alive subdomains... ${RESET}"
-cat $subdomain_path/found.txt | grep $domain | sort -u | httprobe | grep https | tee -a $subdomain_path/alive.txt
+cat $subdomain_path/found.txt | grep $domain | sort -u > $subdomain_path/domain_results.txt
+cat $subdomain_path/found.txt | grep -v $domain | sort -u > $subdomain_path/non-domain_results.txt
+
+# Use httprobe to find the alive subdomains
+cat $subdomain_path/domain_results.txt | httprobe | grep https | tee $subdomain_path/alive.txt
 
 # Use gowitness to take screenshots of the subdomains
 echo -e "${RED} [+] Taking screenshots... ${RESET}"
-gowitness scan file -f $subdomain_path/alive.txt -s $screenshot_path/ --no-http
+gowitness file -f $subdomain_path/alive.txt -P $screenshot_path/ --no-http --disable-db
 
+# cleanup
+rm  $subdomain_path/amass_output.txt  $subdomain_path/found.txt
